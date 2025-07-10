79f9e16b64eb12c82495ce6674eca551faa85d8f
habibuoy <habibiemuhammad90@gmail.com>
2025-07-10 16:05:17 +0700

    Feat: Add registration for all command and query handlers using Scrutor

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -9,6 +9,17 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.Scan(selector => selector.FromAssembliesOf(typeof(DependencyInjection))
+            .AddClasses(filter => filter.AssignableTo(typeof(IQueryHandler<,>)), false)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+            .AddClasses(filter => filter.AssignableTo(typeof(ICommandHandler<>)), false)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime()
+            .AddClasses(filter => filter.AssignableTo(typeof(ICommandHandler<,>)), false)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
         services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly, includeInternalTypes: true);
 
         services.Decorate(typeof(ICommandHandler<>), typeof(ValidationDecorator.CommandHandler<>));
