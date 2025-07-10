e5d1eb4a070c582b424009b8b7c3b16f03238790
habibuoy <habibiemuhammad90@gmail.com>
2025-07-10 14:40:04 +0700

    Chore: Add FluentValidationExtensions package

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -6,6 +7,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly, includeInternalTypes: true);
         return services;
     }
 }
